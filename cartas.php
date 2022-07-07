c5701c2dca54cd132cdc714afc13eae29e26ac05
CandelasHernandezJossueAlejandro <al222110811@gmail.com>
2022-07-06 22:08:03 -0500

    Desarrollo de index, cabecera y falta unir las tarjetas

---
+++ cartas.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" type="text/css" href="./styles/stylescard.css">
+
+<section class="cards">
+<article class="card card--1">
+  
+  <div class="card__img"></div>
+  <a href="#" class="card_link">
+     <div class="card__img--hover"></div>
+   </a>
+  <div class="card__info">
+    <h3 class="card__title">Cuidado</h3>
+    <p>Buscamos que usted tenga la seguridad sobre su mascota y que reciba la mejor experiencia posible</p>
+  </div>
+</article>
+  
+  <article class="card card--2">
+  
+  <div class="card__img"></div>
+  <a href="#" class="card_link">
+     <div class="card__img--hover"></div>
+   </a>
+  <div class="card__info">
+    <h3 class="card__title">Los mejores productos</h3>
+    <p>Nuestros servicios son tratados con los productos mejor valorados y de las mejores empresas en el mercado</p>
+  </div>
+</article>
+  <article class="card card--3">
+  
+  <div class="card__img"></div>
+  <a href="#" class="card_link">
+     <div class="card__img--hover"></div>
+   </a>
+  <div class="card__info">
+    <h3 class="card__title">Accesibilidad</h3>
+    <p>Realice sus citas desde la comodidad de su computadora y a la hora que usted desee</p>
+  </div>
+</article>
+</section>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Breaking Rules</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand mb-0 h1" href="#">
+            <i class="fa-solid fa-block-brick" alt="" width="30" height="24" class="d-inline-block align-text-top"></i></i>
+            <img src="./images/icons/ladrillos.png" alt="" width="25" height="24" class="d-inline-block align-text-top">
+            Breaking Rules
+          </a>
+        </div>
+      </nav>
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<incl>
+</html>
